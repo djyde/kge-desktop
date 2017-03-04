c6b500a015799c7e49bb42fd2defc114960246a0
Randy <randypriv@gmail.com>
2017-03-04 21:06:38 +0800

    init

---
+++ index.js
@@ -0,0 +1,69 @@
+const electron = require('electron')
+const path = require('path')
+const url = require('url')
+
+const app = electron.app
+
+let mainWindow
+let isQuitting = false
+
+const isAlreadyRunning = app.makeSingleInstance(() => {
+	if (mainWindow) {
+		if (mainWindow.isMinimized()) {
+			mainWindow.restore()
+		}
+
+		mainWindow.show()
+	}
+})
+
+if (isAlreadyRunning) {
+  app.quit()
+}
+
+const URL = url.format({
+  pathname: path.resolve(__dirname, './src/index.html'),
+  protocol: 'file:',
+  slashes: true
+})
+
+function createMainWindow () {
+  const win = new electron.BrowserWindow({
+    show: false,
+    titleBarStyle: 'hidden-inset'
+  })
+
+  win.loadURL(URL)
+
+  win.on('close', e => {
+    if (!isQuitting) {
+      e.preventDefault()
+
+      if (process.platform === 'darwin') {
+        app.hide()
+      } else {
+        win.hide()
+      }
+    }
+  })
+  return win
+}
+
+app.on('ready', () => {
+
+  mainWindow = createMainWindow()
+
+  const page = mainWindow.webContents
+
+  page.on('dom-ready', () => {
+    mainWindow.show()
+  })
+})
+
+app.on('activate', () => {
+  mainWindow.show()
+})
+
+app.on('before-quit', () => {
+  isQuitting = true
+})
