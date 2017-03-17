f6f2a6882005622d2daadb43f2fec8b23aebd8eb
Randy <randypriv@gmail.com>
2017-03-17 16:14:20 +0800

    sort by createdAt

--- src/store.ts
+++ src/store.ts
@@ -5,11 +5,12 @@ const db = new PouchDB('followings')
 import { User } from './Sidebar'
 
 export const getFollowings = async () => {
-  return (await db.allDocs({ include_docs: true })).rows.map(row => row.doc)
+  return (await db.allDocs({ include_docs: true, descending: 'createdAt' })).rows.map(row => row.doc)
 }
 
 export const addFollowing = (user: User) => {
   return db.put({
+    createdAt: Date.now(),
     _id: user.kge_uid,
     ...user
   })
